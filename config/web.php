904942f2feecf17f2d21f0b0849e88645fb7ab4b
yefulin <2500@3tmall.com>
2015-10-29 10:14:45 +0800

    set 默认控制器为user/login

--- config/web.php
+++ config/web.php
@@ -6,7 +6,8 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
-    'homeUrl' => '/',
+    'homeUrl' => 'user/login',
+    'defaultRoute' => 'user/login',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
