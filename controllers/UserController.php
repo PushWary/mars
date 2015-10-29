9add840b4557227e94dd752d0da10c53de5a8613
trainking <yefulincode@gmail.com>
2015-10-29 18:23:13 +0800

    add 验证数据中的用户名和密码

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -6,6 +6,7 @@ use Yii;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
+use app\models\User;
 
 class UserController extends Controller {
 
@@ -14,8 +15,12 @@ class UserController extends Controller {
     public function actionLogin() {
         $this->layout = false;
         if (isset($_POST['user'])) {
-            var_dump($_POST['user']);
-            echo "post";
+            $checkLogin = User::validateLogin($_POST['user']['username'], $_POST['user']['password']);
+            if ($checkLogin) {
+                return "登录成功";
+            }else {
+                return "登录失败";
+            }
         }
         return $this->render('login');
     }
--- models/User.php
+++ models/User.php
@@ -2,102 +2,26 @@
 
 namespace app\models;
 
-class User extends \yii\base\Object implements \yii\web\IdentityInterface
-{
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
+use yii\db\ActiveRecord;
+use yii\db\Query;
 
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
+class User extends ActiveRecord {
 
-    /**
-     * @inheritdoc
-     */
-    public static function findIdentity($id)
-    {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+    public static function tableName () {
+        return "mars_user";
     }
 
     /**
-     * @inheritdoc
+     * 检查用户登录
+     * @param $username 用户名
+     * @param $password 密码
+     * @return boolean 验证是否通过
      */
-    public static function findIdentityByAccessToken($token, $type = null)
-    {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
+    public static function validateLogin($username, $password) {
+        $query = new Query();
+        $query->from('mars_user');
+        $query->where(['username'=>strip_tags($username), 'password'=>strip_tags($password)]);
 
-        return null;
-    }
-
-    /**
-     * Finds user by username
-     *
-     * @param  string      $username
-     * @return static|null
-     */
-    public static function findByUsername($username)
-    {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getAuthKey()
-    {
-        return $this->authKey;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function validateAuthKey($authKey)
-    {
-        return $this->authKey === $authKey;
-    }
-
-    /**
-     * Validates password
-     *
-     * @param  string  $password password to validate
-     * @return boolean if password provided is valid for current user
-     */
-    public function validatePassword($password)
-    {
-        return $this->password === $password;
+        return $query->exists();
     }
 }
