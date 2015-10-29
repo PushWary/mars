2c9b2010e3ca86480aa06a18f192c35d002ad9ba
trainking <yefulincode@gmail.com>
2015-10-29 16:05:27 +0800

    fix 登录页面显示中文

--- views/user/login.php
+++ views/user/login.php
@@ -22,42 +22,42 @@
         <div id="login" class="tab-pane active">
           <form action="index.html">
             <p class="text-muted text-center">
-              Enter your username and password
+              输入你的用户名和密码
             </p>
-            <input type="text" placeholder="Username" class="form-control top">
-            <input type="password" placeholder="Password" class="form-control bottom">
+            <input type="text" placeholder="用户名" class="form-control top">
+            <input type="password" placeholder="密 码" class="form-control bottom">
             <div class="checkbox">
               <label>
-                <input type="checkbox"> Remember Me
+                <input type="checkbox"> 记住我
               </label>
             </div>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">登录</button>
           </form>
         </div>
         <div id="forgot" class="tab-pane">
           <form action="index.html">
-            <p class="text-muted text-center">Enter your valid e-mail</p>
-            <input type="email" placeholder="mail@domain.com" class="form-control">
+            <p class="text-muted text-center">注册邮箱</p>
+            <input type="email" placeholder="mail@example.com" class="form-control">
             <br>
-            <button class="btn btn-lg btn-danger btn-block" type="submit">Recover Password</button>
+            <button class="btn btn-lg btn-danger btn-block" type="submit">找回密码</button>
           </form>
         </div>
         <div id="signup" class="tab-pane">
           <form action="index.html">
-            <input type="text" placeholder="username" class="form-control top">
-            <input type="email" placeholder="mail@domain.com" class="form-control middle">
-            <input type="password" placeholder="password" class="form-control middle">
-            <input type="password" placeholder="re-password" class="form-control bottom">
-            <button class="btn btn-lg btn-success btn-block" type="submit">Register</button>
+            <input type="text" placeholder="用户名" class="form-control top">
+            <input type="email" placeholder="邮箱" class="form-control middle">
+            <input type="password" placeholder="密码" class="form-control middle">
+            <input type="password" placeholder="重复密码" class="form-control bottom">
+            <button class="btn btn-lg btn-success btn-block" type="submit">注册</button>
           </form>
         </div>
       </div>
       <hr>
       <div class="text-center">
         <ul class="list-inline">
-          <li> <a class="text-muted" href="#login" data-toggle="tab">Login</a>  </li>
-          <li> <a class="text-muted" href="#forgot" data-toggle="tab">Forgot Password</a>  </li>
-          <li> <a class="text-muted" href="#signup" data-toggle="tab">Signup</a>  </li>
+          <li> <a class="text-muted" href="#login" data-toggle="tab">登录</a>  </li>
+          <li> <a class="text-muted" href="#forgot" data-toggle="tab">忘记密码</a>  </li>
+          <li> <a class="text-muted" href="#signup" data-toggle="tab">注册</a>  </li>
         </ul>
       </div>
     </div>
