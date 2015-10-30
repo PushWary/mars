65d6bf0181085dfddcb89979aa5cfe57351da073
trainking <yefuincode@gmail.com>
2015-10-30 13:59:23 +0800

    fix ActiveRecord的形式验证登录数据

--- models/User.php
+++ models/User.php
@@ -18,10 +18,7 @@ class User extends ActiveRecord {
      * @return boolean 验证是否通过
      */
     public static function validateLogin($username, $password) {
-        $query = new Query();
-        $query->from('mars_user');
-        $query->where(['username'=>strip_tags($username), 'password'=>strip_tags($password)]);
 
-        return $query->exists();
+        return static::find()->where(['username'=>strip_tags($username), 'password'=>strip_tags($password)])->exists();
     }
 }
