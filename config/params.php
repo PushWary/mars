ec9f2bf59c64be7a424085511ad01f6e11dd63cd
trainking <yefulincode@gmail.com>
2015-10-30 15:09:34 +0800

    add 用户密码的加密校验及用户状态

--- config/params.php
+++ config/params.php
@@ -2,4 +2,5 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+    'passwordKey' => 'e01eeed093cb22bb',
 ];
--- models/User.php
+++ models/User.php
@@ -2,13 +2,28 @@
 
 namespace app\models;
 
+use Yii;
 use yii\db\ActiveRecord;
 use yii\db\Query;
 
 class User extends ActiveRecord {
 
+    const STATUS_DEF = array(
+        'normal' => 0,  // 未激活
+        'active' => 1,  // 激活
+        );
+
     public static function tableName () {
         return "mars_user";
+    }
+
+    public function beforeSave($insert) {
+        if (parent::beforeSave($insert)) {
+            $this->password = crypt($this->password, Yii::$app->params['passwordKey']);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -19,6 +34,10 @@ class User extends ActiveRecord {
      */
     public static function validateLogin($username, $password) {
 
-        return static::find()->where(['username'=>strip_tags($username), 'password'=>strip_tags($password)])->exists();
+        return static::find()->where([
+            'username' => strip_tags($username), 
+            'password' => crypt(strip_tags($password), Yii::$app->params['passwordKey']),
+            'status' => self::STATUS_DEF['active'],
+            ])->exists();
     }
 }
