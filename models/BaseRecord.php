4c3d4cab0414bf2876ff66de3760911cc380aeb1
trainking <yefulincode@gmail.com>
2015-11-02 16:10:12 +0800

    add 基础record操作类
    1. 定义获取UUID方法
    
    2. 保存操作中加入created（创建时间）的预处理

---
+++ models/BaseRecord.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+use yii\db\Query;
+
+class BaseRecord extends ActiveRecord {
+
+    public function beforeSave($insert) {
+        if (parent::beforeSave($insert)) {
+            $this->created = time();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 获取全局唯一的uuid
+     */
+    public function getUUID() {
+        $command = Yii::$app->db->createCommand("SELECT UUID()");
+        $value = $command->queryScalar();
+        return $value;
+    }
+}
