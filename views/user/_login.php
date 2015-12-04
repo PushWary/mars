9ce702bd1f86e2f0ecbf3e8f4f326b575752ba27
yield <yefulincode@gmail.com>
2015-12-04 15:50:13 +0800

    fix 登录也的title

--- views/user/_login.php
+++ views/user/_login.php
@@ -2,7 +2,7 @@
 <div id="login" class="tab-pane active" ng-controller='LoginCtrl'>
   <form action="/user/login" method="POST">
     <p class="text-muted text-center" >
-        {{ message }}
+        输入用户名和密码
     </p>
     <input type="text" placeholder="用户名" class="form-control top" name="LoginForm[username]" required maxlength="32"
         pattern="[0-9a-zA-Z!@#^&*()_+{}|:?\-=\[\].]{5,32}" title="字母,数字,下划线,长度5~32">
