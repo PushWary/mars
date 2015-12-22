14af8aba7a0ef89d5c4c0d7b1e5ff1b313c1db0c
yield <yefulincode@gmail.com>
2015-12-22 10:07:30 +0800

    增加获取当前用户ip和匹配所在地区的方法

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -34,4 +34,37 @@ class BaseController extends Controller {
             return false;
         }
     }
+
+    /**
+     * 获取当前用户的ip
+     */
+    public function getIP() {
+        if(!empty($_SERVER["HTTP_CLIENT_IP"])) {
+            $cip = $_SERVER["HTTP_CLIENT_IP"];
+        }else if (!empty($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+            $cip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+        }else if (!empty($_SERVER["REMOTE_ADDR"])) {
+            $cip = $_SERVER["REMOTE_ADDR"];
+        }else {
+            $cip = '';
+        }
+
+        preg_match("/[\d\.]{7,15}/", $cip, $cips);
+        $cip = isset($cips[0]) ? $cips[0] : 'unknown';
+        unset($cips);
+        return $cip;
+    }
+
+    /**
+     * 根据IP获取用户所在地址
+     */
+    public function lazdf($ip) {
+        $curl = curl_init();
+        curl_setopt($curl,CURLOPT_URL,"http://www.ip138.com/ips138.asp?ip=".$ip);
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
+        curl_close($curl);
+        preg_match("/<ul class=\"ul1\"><li>(.*?)<\/li>/i",$ipdz,$jgarray);
+        preg_match("/本站主数据：(.*)/i", $jgarray[1],$ipp);
+        return  "<div class=\"global_widht global_zj zj\" style=\"background: none repeat scroll 0% 0% rgb(226, 255, 191); font-size: 12px; color: rgb(85, 85, 85); height: 30px; line-height: 30px; border-bottom: 1px solid rgb(204, 204, 204); text-align: left;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;欢迎来自&nbsp;<b>".$ipp[1]."</b>&nbsp;的朋友！</div>";
+    }
 }
