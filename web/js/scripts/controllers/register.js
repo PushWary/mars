7ba4057f0929d3760feaa8257d8b319cb2acedfe
yield <yefulincode@gmail.com>
2015-12-25 17:21:26 +0800

    修改为angular-ui-router控制页面的显示

--- web/js/scripts/controllers/register.js
+++ web/js/scripts/controllers/register.js
@@ -11,20 +11,21 @@
             user['user'].username = name;
             user['user'].email = email;
             user['password'].password = password;
+            console.log(password != rePassword);
             if (password != rePassword) {
                 $scope.show = true;
                 $scope.alertMsg = "两次输入的密码不一致";
                 return false;
             }
 
-            UserService.register(user).then(function(result) {
-                var data = result.data;
-                if(data.success === 1) {
-                    location.href = 'user/index';
-                }else {
-                    // TODO 登录失败提示
-                }
-            });
+            // UserService.register(user).then(function(result) {
+            //     var data = result.data;
+            //     if(data.success === 1) {
+            //         location.href = 'user/index';
+            //     }else {
+            //         // TODO 登录失败提示
+            //     }
+            // });
         };
     }
 })();
--- web/js/scripts/marsApp.js
+++ web/js/scripts/marsApp.js
@@ -1,5 +1,19 @@
 (function() {
     'use strict';
 
-    angular.module('marsApp', []);
+    var myApp = angular.module('marsApp', ['ui.router']);
+
+    myApp.config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('login', {
+            url: "/user/login",
+            templateUrl: "/view/user/login.html"
+        }).state('lostpwd', {
+            url: "/user/lostpwd",
+            templateUrl: "/view/user/lostpwd.html"
+        }).state("register", {
+            url: "/user/register",
+            templateUrl: "/view/user/register.html"
+        });
+    });
+
 })();
