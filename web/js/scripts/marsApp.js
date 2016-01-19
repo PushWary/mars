9baeb0200ecb3cfcfdcacda6bca6c714de3b58d1
yield <yefulincode@gmail.com>
2016-01-19 14:08:07 +0800

    修改前端路由代码的位置
    rt:抽取到routes.js

--- web/js/scripts/marsApp.js
+++ web/js/scripts/marsApp.js
@@ -2,18 +2,4 @@
     'use strict';
 
     var myApp = angular.module('marsApp', ['ui.router']);
-
-    myApp.config(function($stateProvider, $urlRouterProvider) {
-        $stateProvider.state('login', {
-            url: "/user/login",
-            templateUrl: "/view/user/login.html"
-        }).state('lostpwd', {
-            url: "/user/lostpwd",
-            templateUrl: "/view/user/lostpwd.html"
-        }).state("register", {
-            url: "/user/register",
-            templateUrl: "/view/user/register.html"
-        });
-    });
-
 })();
--- web/js/scripts/routes.js
+++ web/js/scripts/routes.js
@@ -1,11 +1,16 @@
 (function() {
     'use strict';
 
-    angular.module('marsApp').config(['$locationProvider', function($locationProvider) {
-
-        $locationProvider.html5Mode({
-            enabled : true,
-            requireBase : false
+    angular.module('marsApp').config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('login', {
+            url: "/user/login",
+            templateUrl: "/view/user/login.html"
+        }).state('lostpwd', {
+            url: "/user/lostpwd",
+            templateUrl: "/view/user/lostpwd.html"
+        }).state("register", {
+            url: "/user/register",
+            templateUrl: "/view/user/register.html"
         });
-    }]);
+    });
 })();
