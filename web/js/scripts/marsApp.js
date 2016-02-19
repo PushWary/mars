4a6ce243827d9d2c197044681349b5caa3247f2c
yield <yefulincode@gmail.com>
2016-02-19 15:45:43 +0800

    增加http拦截器

--- web/js/scripts/marsApp.js
+++ web/js/scripts/marsApp.js
@@ -2,4 +2,33 @@
     'use strict';
 
     var myApp = angular.module('marsApp', ['ui.router']);
+
+    // http 拦截器
+    myApp.factory('httpInterceptor', ['$q', '$injector', function($q, $injector) {
+        var httpInterceptor = {
+            'responseError': function(response) {
+                switch (response.status) {
+                    case 404:
+                        alert('404!');
+                        break;
+                }
+                return $q.reject(response);
+            },
+            'response': function(response) {
+                return response;
+            },
+            'request': function(config) {
+                return config;
+            },
+            'requestError': function(config) {
+                return $q.reject(config);
+            }
+        };
+        return httpInterceptor;
+    }]);
+
+    // 将拦截器注册
+    myApp.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.interceptors.push('httpInterceptor');
+    }]);
 })();
