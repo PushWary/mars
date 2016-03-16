b22d95d8d083514d7d3c87ad6f3efef10fe432b6
yield <yefulincode@gmail.com>
2016-03-16 22:27:37 +0800

    add 压缩缩略图片

--- models/UploadForm.php
+++ models/UploadForm.php
@@ -4,6 +4,9 @@ namespace app\models;
 use Yii;
 use yii\base\Model;
 use yii\web\UploadedFile;
+use Imagine\Gd\Imagine;
+use Imagine\Image\Box;
+use Imagine\Image\ImageInterface;
 
 class UploadForm extends model {
 
@@ -22,17 +25,19 @@ class UploadForm extends model {
         if ($this->validate()) {
             $name = $this->imageFile->name;
             $fileType = substr($name, strripos($name,'.')+1);
-            $path = 'uploads/'.$this->imageFile->baseName . '.'. $fileType;  // 保存文件路劲
+            $path = 'uploads/'.$this->imageFile->baseName . '.'. $fileType;  // 保存文件路径
+            $avator = 'avator/'.$this->imageFile->baseName . '.'. $fileType;  // 缩略图路径
             $transtion =  Yii::$app->db->beginTransaction();
 
             // 获取当前用户
             $user = User::find()->where(['id'=>Yii::$app->user->getId()])->one();
             try {
-                $user->avator = $path;  // 保存文件路径到数据表中
+                $this->imageFile->saveAs($path);
+                $this->thumbnailImage($path, $avator);   // 压缩缩略图片
+                $user->avator = $avator;  // 保存文件路径到数据表中
                 if (!$user->save()) {
                     return false;
                 }
-                $this->imageFile->saveAs($path);
                 $transtion->commit();
             } catch (Exception $e) {
                 return false;
@@ -42,6 +47,21 @@ class UploadForm extends model {
         } else {
             return false;
         }
+    }
+
+    /**
+     * 缩略图生成函数
+     * @param $sourcPath 源路径
+     * @param $outPath 输出路径
+     */
+    private function thumbnailImage($sourcPath, $outPath) {
+        $imagine = new Imagine();
+        $size = new Box(40, 40);
+        $mode = ImageInterface::THUMBNAIL_OUTBOUND;
+
+        $imagine->open($sourcPath)
+            ->thumbnail($size, $mode)
+            ->save($outPath);
     }
 }
 ?>
