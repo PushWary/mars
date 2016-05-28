5058c259036781a57b4c66a5ae7a8118b1e5128d
yield <yefulincode@gmail.com>
2016-05-28 23:03:44 +0800

    增加bootstrap-fileinput的代码注释

--- web/js/scripts/controllers/myInfo.js
+++ web/js/scripts/controllers/myInfo.js
@@ -12,14 +12,15 @@
                     // 当dialog被打开时调用
                     $rootScope.$on('ngDialog.opened', function(e, $dialog) {
                         $("#input-id").fileinput({
-                            showClose: false,
-                            showCaption: false,
-                            language: "zh",
-                            uploadUrl: "/users/avator",
-                            defaultPreviewContent: "<img class='media-object img-thumbnail user-img' alt='头像' src='/img/user.gif' style='width:218px'>",
-                            browseLabel: '选择图片',
-                            browseIcon: "<i class='glyphicon glyphicon-folder-open'></i>",
-                            allowedFileExtensions: ["jpg", "png", "gif"],
+                            showClose: false,     // 是否显示每个图片右上角的图片
+                            showCaption: true,   //  是否显示file空间的路径框
+                            language: "zh",     // 语言
+                            uploadUrl: "/users/avator",   // 上传文件路径
+                            defaultPreviewContent:
+                                "<img class='media-object img-thumbnail user-img' alt='头像' src='/img/user.gif' style='width:218px'>",  // 默认预览
+                            browseLabel: '选择图片',  // file控件的文本
+                            browseIcon: "<i class='glyphicon glyphicon-folder-open'></i>",  // file控件的图标
+                            allowedFileExtensions: ["jpg", "png", "gif"],   // 限制上传文件类型
                             // 设置额外上传数据
                             uploadExtraData: {
                                 _csrf: $("meta[name='csrf-token']").attr("content")
