eede8b21e15bea8f50e75f645a702a6c8208213e
yield <yefulincode@gmail.com>
2016-07-09 17:39:51 +0800

    通过angular.js设置验证参数

--- web/js/scripts/controllers/login.js
+++ web/js/scripts/controllers/login.js
@@ -4,12 +4,20 @@
     angular.module('marsApp').controller('LoginCtrl', ['$location', '$scope', 'UserService', LoginCtrl]);
 
     function LoginCtrl($location, $scope, UserService) {
+
+        // 登录表单
         $scope.loginForm = {
             'LoginForm': {
                 'username': '',
                 'password': '',
                 'remberMe': false
             }
+        };
+
+        // 用户名和密码验证器
+        $scope.passwordValidate = {
+            pattern : "[A-za-z]+\\w{5,32}",
+            title: "字母和数字,以字母开始,长度5~32"
         };
 
         // 表单中输入值发生改变
